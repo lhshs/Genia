e3e9a55ee6d2cf31212fdb0f82ac17947cbb655a
lhshs <hs.lion2@gmail.com>
2023-12-14 14:42:50 +0900

    Reset log & push

---
+++ Final_pjt2/Dash/docker/app.py
@@ -0,0 +1,273 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import base64
+import io
+import psycopg2
+
+import pandas as pd
+import plotly.express as px
+
+from moviepy.editor import VideoFileClip
+import dash_table
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import plotly.express as px
+from wordcloud import WordCloud
+import plotly.graph_objects as go
+from matplotlib import pyplot as plt
+# plt.rcParams['font.family'] ='Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] =False
+plt.rc('font', family='NanumGothic')
+plt.rc('font', family='NanumBarunGothic')
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import networkx as nx
+import tempfile
+from figure import Figure_Creator
+
+
+app = dash.Dash()
+
+figure_creator = Figure_Creator('../data/Iris.csv', '../Lecture_Text/summarize/손석민/LECTURE_GPT_ALL_KOR_41672.txt')
+
+
+app.layout = html.Div([
+    # Title
+    html.Div(
+        children=[
+            html.H1('Video Analysis',
+                    style={'textAlign': 'center'}),
+        ]),
+
+    # Upload Video
+    html.Div(
+        dcc.Upload(
+            id='upload-video',
+            children=html.Div([
+                'Drag and Drop or Select a Video',
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded        
+            multiple=True,
+        ),
+        style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}
+    ),
+
+    # Output Video
+    html.Div(id='output-video',
+             style={'display': 'flex', 
+                    'justifyContent': 'space-around', 
+                    'alignItems': 'center'}),
+    
+    html.Br(),
+    html.Br(),
+'''
+    # feature 1
+    html.Div(id='feature1',
+             children=[
+                 html.H2('Face Detection And Mesh'),
+                 dcc.Graph(id='NonVerbal_graph1')
+             ]),
+    html.Br(),
+'''
+    # # feature 2
+    # html.Div(id='feature2',
+    #          children=[
+    #              html.H2('Sentimental Analysis'),
+    #              dcc.Graph(id='NonVerbal_graph2')
+    #          ]),   
+
+    # html.Br(),
+
+    # # feature 3
+    # html.Div(id='feature3',
+    #          children=[
+    #              html.H2('Pose Detection'),
+    #              dcc.Graph(id='NonVerbal_graph3')
+    #          ]),  
+
+    # html.Br(),
+
+    # # feature 4
+    # html.Div(id='feature4',
+    #          children=[
+    #              html.H2('Face & Arm Detection'),
+    #              dcc.Graph(id='NonVerbal_graph4')
+    #          ]),   
+'''
+    html.Br(),
+
+    # NLP_feature 1
+    html.Div(id='NLP_feature 1',
+             children=[
+                 html.H2('The Most Frequent Words'),
+                 dcc.Graph(id='NLP_graph1'
+                           )
+             ]),   
+             '''
+    # # NLP_feature 2
+    # html.Div(id='NLP_feature 2',
+    #          children=[
+    #              html.H2('Histogram Of Word Length'),
+    #              dcc.Graph(id='NLP_graph2')
+    #          ]),   
+    # # NLP_feature 3
+    # html.Div(id='NLP_feature 3',
+    #          children=[
+    #              html.H2('Heatmap Of Term Frequency'),
+    #              dcc.Graph(id='NLP_graph3')
+    #          ]),   
+    # # NLP_feature 4
+    # html.Div(id='NLP_feature 4',
+    #          children=[
+    #              html.H2('Network Diagram'),
+    #              dcc.Graph(id='NLP_graph4')
+    #          ]),                                           
+
+])
+
+
+@app.callback(
+    Output('output-video', 'children'),
+    Input('upload-video', 'contents'),
+    prevent_initial_call=True
+)
+def side_data(list_of_contents):
+    fig = None
+    if list_of_contents is not None:
+        for contents in list_of_contents:
+            # The contents are base64 encoded, so we must decode them
+            decoded = base64.b64decode(contents.split(',')[1])
+
+            # Write the decoded video data to a temporary file
+            with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as tmpfile:
+                tmpfile.write(decoded)
+                tmpfilepath = tmpfile.name
+
+            # Read the video file from the temporary file
+            clip = VideoFileClip(tmpfilepath)
+
+            # Extract metadata
+            duration = clip.duration
+            fps = clip.fps
+            size = str(clip.size)
+            audio_fps = clip.audio.fps if clip.audio is not None else None
+            audio_nchannels = clip.audio.nchannels if clip.audio is not None else None
+        
+            ########### About Side Table ###########
+            # Create a DataFrame from the metadata
+            df = pd.DataFrame({
+                'Duration': [duration],
+                'FPS': [fps],
+                'Size': [size],
+                'Audio FPS': [audio_fps],
+                'Audio Channels': [audio_nchannels],
+            })
+
+            # Transpose the DataFrame
+            df = df.transpose()
+            df.columns = ['Value']            
+
+            # Reset the index and rename the index column to 'Metadata'
+            df = df.reset_index().rename(columns={'index': 'Metadata'})            
+
+            # Create a DataTable from the DataFrame
+            table = go.Figure(data=[go.Table(
+                header=dict(values=list(df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[df.Metadata, df.Value],
+                           fill_color='lavender',
+                           align='left'))
+            ])                
+
+        children = [
+            html.Div(
+                children=[
+                    html.Video(src=contents, 
+                               controls=True, 
+                               style={'width': '100%'})
+                ],
+                style={'width': '50%'}
+            ),
+            html.Div(
+                children=[dcc.Graph(figure=table)],
+                style={'width': '70%', 'alignItems': 'center'}
+            )
+        ]
+        return children
+'''
+@app.callback(
+    Output('NonVerbal_graph1', 'figure'),
+    Input('upload-video', 'contents'),
+    prevent_initial_call=True
+)
+def update_graph(n):
+    fig = None
+    if n is not None:    
+        fig = figure_creator.create_sample()
+    return fig
+
+
+@app.callback(
+    Output('NLP_graph1', 'figure'),
+    Input('upload-video', 'contents'),
+    prevent_initial_call=True
+)
+def update_graph(n):
+    fig = None
+    if n is not None:    
+        fig = figure_creator.create_bar()
+    return fig
+    
+'''
+# @app.callback(
+#     Output('NLP_graph2', 'figure'),
+#     Input('upload-video', 'contents'),
+#     prevent_initial_call=True
+# )
+# def update_graph(n):
+#     fig = None
+#     if n is not None:    
+#         fig = figure_creator.create_hist()
+#     return fig
+    
+    
+# @app.callback(
+#     Output('NLP_graph3', 'figure'),
+#     Input('upload-video', 'contents'),
+#     prevent_initial_call=True
+# )
+# def update_graph(n):
+#     fig = None
+#     if n is not None:    
+#         fig = figure_creator.create_heatmap()
+#     return fig
+    
+    
+# @app.callback(
+#     Output('NLP_graph4', 'figure'),
+#     Input('upload-video', 'contents'),
+#     prevent_initial_call=True
+# )
+# def update_graph(n):
+#     fig = None
+#     if n is not None:    
+#         fig = figure_creator.create_network()
+#     return fig
+
+if __name__ == '__main__':
+    app.run_server(port=80, debug=True)
---
+++ Final_pjt2/Dash/figure.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import plotly.express as px
+import nltk    
+
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+import networkx as nx
+import plotly.graph_objects as go
+
+from data import Figure_Data
+
+
+class Figure_Creator:
+    def __init__(self, data_path, text_path):
+        self.data = Figure_Data(data_path, text_path)
+
+    # Sample Figure
+    def create_sample(self):
+        feature, processed_text = self.data.get_processed_data()
+        fig = px.scatter(feature, x="SepalWidth", y="SepalLength", color="Species")
+        return fig
+
+    # Create a bar plot of the most frequent words
+    def create_bar(self):
+        feature, processed_text = self.data.get_processed_data()
+        word_counts = Counter(processed_text)
+        most_common_words = word_counts.most_common(10)
+        fig = px.bar(x=[word[0] for word in most_common_words], y=[word[1] for word in most_common_words])
+        return fig
+    
+    # Create a histogram of word length
+    def create_hist(self):
+        feature, processed_text = self.data.get_processed_data()
+        word_lengths = [len(word) for word in processed_text]
+        fig = px.histogram(x=word_lengths, nbins=max(word_lengths))
+        return fig
+
+    # Create a heatmap of term frequency
+    def create_heatmap(self):
+        feature, processed_text = self.data.get_processed_data()
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(processed_text)
+        term_frequency = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
+        fig = px.imshow(term_frequency)
+        return fig
+
+    # Create a network diagram
+    def create_network(self):
+        feature, processed_text = self.data.get_processed_data()
+        bigrams = nltk.bigrams(processed_text)
+        bigram_freq = nltk.FreqDist(bigrams)
+        bigram_df = pd.DataFrame(bigram_freq.items(), columns=["bigram", "freq"])
+        bigram_df = bigram_df.sort_values(by="freq", ascending=False)
+
+        G = nx.Graph()
+
+        for index, row in bigram_df.iterrows():
+            G.add_edge(row["bigram"][0], row["bigram"][1], weight=row["freq"])
+
+        pos = nx.spring_layout(G)
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.append(x0)
+            edge_x.append(x1)
+            edge_x.append(None)
+            edge_y.append(y0)
+            edge_y.append(y1)
+            edge_y.append(None)
+
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        for node in G.nodes():
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=True,
+                colorscale='YlGnBu',
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar=dict(
+                    thickness=15,
+                    title='Node Connections',
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=2))
+
+        node_adjacencies = []
+        node_text = []
+        for node, adjacencies in enumerate(G.adjacency()):
+            node_adjacencies.append(len(adjacencies[1]))
+            node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+        node_trace.marker.color = node_adjacencies
+        node_trace.text = node_text
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        # title='Network graph made with Python',
+                        titlefont_size=16,
+                        showlegend=False,
+                        hovermode='closest',
+                        margin=dict(b=20,l=5,r=5,t=40),
+                        # annotations=[ dict(
+                        #     showarrow=False,
+                        #     xref="paper", yref="paper",
+                        #     x=0.005, y=-0.002 ) ],
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )        
+
+        return fig
